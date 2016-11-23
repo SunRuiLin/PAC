98991005b992ce2abfb565276ef3c7640e9c2872
RuiLin Sun <781717876@qq.com>
2016-11-23 18:32:03 +0800

    update

---
+++ 51070.js
@@ -0,0 +1,19 @@
+var proxy = "SOCKS5 localhost:51070", localhost = [
+    "127.0.0.1",
+    "192.168.1.1",
+    "localhost"
+];
+/**
+ * 入口函数
+ * @param url URL
+ * @param host HOST
+ * @return {string} 返回代理
+ * @constructor
+ */
+var FindProxyForURL = function (url, host) {
+    0 && FindProxyForURL && void url;
+    if (localhost.indexOf(host) !== -1) {
+        return "DIRECT";
+    }
+    return proxy;
+};
---
+++ 51070.ts
@@ -0,0 +1,23 @@
+const proxy = "SOCKS5 localhost:51070",
+    localhost = [
+        "127.0.0.1",
+        "192.168.1.1",
+        "localhost"
+    ];
+
+/**
+ * 入口函数
+ * @param url URL
+ * @param host HOST
+ * @return {string} 返回代理
+ * @constructor
+ */
+const FindProxyForURL = (url: string, host: string) => {
+    0 && FindProxyForURL && void url;
+
+    if (localhost.indexOf(host) !== -1) {
+        return "DIRECT";
+    }
+
+    return proxy;
+};
