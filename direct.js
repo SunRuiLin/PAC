c713d87135d69daa7e7a556b78bf233826ab81e4
RuiLin Sun <781717876@qq.com>
2017-04-22 18:19:03 +0800

    update

--- direct.js
+++ direct.js
@@ -1,6 +1,8 @@
 var proxy = "SOCKS5 localhost:51080", host_rules = [
     // IP138
     "*.ip138.com",
+    // Investopedia
+    "*.investopedia.com",
     // Microsoft
     "*.live.com",
     // Read The Docs
--- direct.ts
+++ direct.ts
@@ -3,6 +3,9 @@ const proxy = "SOCKS5 localhost:51080",
         // IP138
         "*.ip138.com",
 
+        // Investopedia
+        "*.investopedia.com",
+
         // Microsoft
         "*.live.com",
 
